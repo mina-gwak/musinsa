2c5c17cf84c6e4f20bf387e22e0c02b54efbe3fc
mina-gwak <mina.gwak@kakao.com>
2022-07-15 03:22:07 +0900

    feat: SearchField 컴포넌트 생성 (mina-gwak/musinsa#3)

---
+++ src/components/Header/SearchField/index.tsx
@@ -0,0 +1,16 @@
+import * as S from '@components/Header/SearchField/SearchField.style';
+import Icon from '@components/common/Icon';
+import { ICON_NAME, ICON_SIZE } from '@components/common/Icon/constants';
+
+const SearchField = () => {
+  return (
+    <S.SearchField>
+      <S.SearchInputBox>
+        <Icon iconName={ICON_NAME.SEARCH} iconSize={ICON_SIZE.LARGE} />
+        <S.SearchInput type='text' placeholder='상품명 검색' />
+      </S.SearchInputBox>
+    </S.SearchField>
+  );
+};
+
+export default SearchField;
