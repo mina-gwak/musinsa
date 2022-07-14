f3040f026908e498064da53de17513ed7a0492d0
mina-gwak <mina.gwak@kakao.com>
2022-07-15 00:28:12 +0900

    feat: Icon 파일 세팅, Icon 컴포넌트 생성 (mina-gwak/musinsa#3)

---
+++ src/components/common/Icon/Icon.style.ts
@@ -0,0 +1,43 @@
+import SVG from 'react-inlinesvg';
+import styled, { css } from 'styled-components';
+
+import { ICON_SIZE } from '@components/common/Icon/constants';
+
+type StyledIconProps = {
+  size: string;
+};
+
+const sizeStyles = css<StyledIconProps>`
+  ${({ size }) =>
+    size === ICON_SIZE.SMALL &&
+    css`
+      width: 0.875rem;
+      height: 0.875rem;
+    `}
+
+  ${({ size }) =>
+    size === ICON_SIZE.MEDIUM &&
+    css`
+      width: 1.125rem;
+      height: 1.125rem;
+    `}
+
+  ${({ size }) =>
+    size === ICON_SIZE.LARGE &&
+    css`
+      width: 1.375rem;
+      height: 1.375rem;
+    `}
+
+  ${({ size }) =>
+    size === ICON_SIZE.X_LARGE &&
+    css`
+      width: 5.625rem;
+      height: 5.625rem;
+    `}
+`;
+
+export const Icon = styled(SVG)<StyledIconProps>`
+  flex-shrink: 0;
+  ${sizeStyles}
+`;
---
+++ src/components/common/Icon/constants.ts
@@ -0,0 +1,13 @@
+export const ICON_NAME = {
+  CLOSE: 'Close',
+  EMPTY: 'Empty',
+  REFRESH: 'Refresh',
+  SEARCH: 'Search',
+} as const;
+
+export const ICON_SIZE = {
+  SMALL: 'SMALL',
+  MEDIUM: 'MEDIUM',
+  LARGE: 'LARGE',
+  X_LARGE: 'X_LARGE',
+} as const;
