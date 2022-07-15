ac55071babe809da624ed70b0e37f2da21058d6e
mina-gwak <mina.gwak@kakao.com>
2022-07-16 03:03:04 +0900

    feat: 검색창 리셋 버튼 구현 (mina-gwak/musinsa#10)

--- src/components/Header/SearchField/index.tsx
+++ src/components/Header/SearchField/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { useRecoilValue } from 'recoil';
 
 import * as S from '@components/Header/SearchField/SearchField.style';
@@ -6,13 +8,27 @@ import { ICON_NAME, ICON_SIZE } from '@components/common/Icon/constants';
 import { isSearchFieldActiveState } from '@store/searchField';
 
 const SearchField = () => {
+  const [value, setValue] = useState('');
   const isSearchFieldActive = useRecoilValue(isSearchFieldActiveState);
+
+  const isValueExist = value.length > 0;
+
+  const resetValue = () => setValue('');
 
   return isSearchFieldActive ? (
     <S.Container>
       <S.SearchInputBox>
         <Icon iconName={ICON_NAME.SEARCH} iconSize={ICON_SIZE.LARGE} />
-        <S.SearchInput type='text' placeholder='상품명 검색' autoFocus />
+        <S.SearchInput
+          type='text'
+          placeholder='상품명 검색'
+          autoFocus
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+        <S.ResetButton type='button' isValueExist={isValueExist} onClick={resetValue}>
+          <Icon iconName={ICON_NAME.CLOSE} iconSize={ICON_SIZE.SMALL} />
+        </S.ResetButton>
       </S.SearchInputBox>
     </S.Container>
   ) : null;
