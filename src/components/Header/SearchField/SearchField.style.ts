796d642df792806ef2dc0793b2a63d4cb6e00b01
mina-gwak <mina.gwak@kakao.com>
2022-07-16 03:03:04 +0900

    feat: 검색창 리셋 버튼 구현 (mina-gwak/musinsa#10)

--- src/components/Header/SearchField/SearchField.style.ts
+++ src/components/Header/SearchField/SearchField.style.ts
@@ -31,3 +31,26 @@ export const SearchInput = styled.input`
     border-color: ${({ theme }) => theme.colors.grey2};
   }
 `;
+
+export const ResetButton = styled.button<{ isValueExist: boolean }>`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  width: 40px;
+  height: 40px;
+  background: radial-gradient(
+    circle,
+    #e5e5e5 0,
+    #e5e5e5 34%,
+    rgba(229, 229, 229, 0) 34%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  opacity: ${({ isValueExist }) => (isValueExist ? '1' : '0')};
+  visibility: ${({ isValueExist }) => (isValueExist ? 'visible' : 'hidden')};
+  transform: translateY(-50%);
+  transition: all 300ms;
+
+  svg {
+    left: 13px;
+  }
+`;
