c60a5126d9b9dc34fdcd4ded78c4e0c237762c73
mina-gwak <mina.gwak@kakao.com>
2022-07-16 01:05:47 +0900

    feat: 검색창 토글 기능 구현 (mina-gwak/musinsa#9)

--- src/components/Header/SearchField/index.tsx
+++ src/components/Header/SearchField/index.tsx
@@ -1,16 +1,21 @@
+import { useRecoilValue } from 'recoil';
+
 import * as S from '@components/Header/SearchField/SearchField.style';
 import Icon from '@components/common/Icon';
 import { ICON_NAME, ICON_SIZE } from '@components/common/Icon/constants';
+import { isSearchFieldActiveState } from '@store/searchField';
 
 const SearchField = () => {
-  return (
+  const isSearchFieldActive = useRecoilValue(isSearchFieldActiveState);
+
+  return isSearchFieldActive ? (
     <S.Container>
       <S.SearchInputBox>
         <Icon iconName={ICON_NAME.SEARCH} iconSize={ICON_SIZE.LARGE} />
-        <S.SearchInput type='text' placeholder='상품명 검색' />
+        <S.SearchInput type='text' placeholder='상품명 검색' autoFocus />
       </S.SearchInputBox>
     </S.Container>
-  );
+  ) : null;
 };
 
 export default SearchField;
