2bc612f3eabbed8c8a5f1b6d1aadef327c1849cb
mina-gwak <mina.gwak@kakao.com>
2022-07-15 22:30:56 +0900

    feat: ProductSearch 페이지 추가 (mina-gwak/musinsa#2)

---
+++ src/pages/ProductSearch/index.tsx
@@ -0,0 +1,13 @@
+import Header from '@components/Header';
+import ProductList from '@components/ProductList';
+
+const ProductSearch = () => {
+  return (
+    <>
+      <Header />
+      <ProductList />
+    </>
+  );
+};
+
+export default ProductSearch;
