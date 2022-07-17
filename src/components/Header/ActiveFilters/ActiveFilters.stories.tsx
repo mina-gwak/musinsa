5a070c28b0fcf6b6c59145b0aa6a9cf2df1b8f37
mina-gwak <mina.gwak@kakao.com>
2022-07-17 17:02:23 +0900

    fix: recoil 사용하는 스토리 수정 (mina-gwak/musinsa#20)

--- src/components/Header/ActiveFilters/ActiveFilters.stories.tsx
+++ src/components/Header/ActiveFilters/ActiveFilters.stories.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
+
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { useSetRecoilState } from 'recoil';
 
 import ActiveFilters from '@components/Header/ActiveFilters';
+import { activeFilterState } from '@store/filter';
 
 export default {
   title: 'Header/ActiveFilters',
   component: ActiveFilters,
 } as ComponentMeta<typeof ActiveFilters>;
 
-export const Default: ComponentStory<typeof ActiveFilters> = () => <ActiveFilters />;
+export const Default: ComponentStory<typeof ActiveFilters> = () => {
+  const setActiveFilter = useSetRecoilState(activeFilterState);
+
+  useEffect(() => {
+    setActiveFilter({ filter: ['isSoldOut'], search: [] });
+  }, []);
+
+  return <ActiveFilters />;
+};
